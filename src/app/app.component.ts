9e6fdb8494ae06e05190f3aa6d1ac2702bc1e96a
Sherly Chan <sherlyc@gmail.com>
2017-06-24 23:58:33 +1200

    on click selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,30 +72,33 @@ const HEROES: Hero[] = [
 `]
   template: 
   `<h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+   <h2>My Heroes</h2>
+   <ul class="heroes">
+     <li *ngFor="let hero of heroes" 
+    (click) ="onSelect(hero)" [class.selected]="hero === selectedHero">
       <span class="badge">{{hero.id}}</span>
       {{hero.name}}
     </li>
   </ul>
-  <h2>{{hero.name}} details!</h2>
-  <div>
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div>
     <label>id: </label>
-    {{hero.id}}
-  </div>
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]="hero.name" placeholder="name">
-  {{hero.name}}
+      {{selectedHero.id}}
+    </div>
+    <div>
+    <label>name: </label>
+    <input [(ngModel)]="selectedHero.name" placeholder="name">
+      {{selectedHero.name}}
+    </div>
   </div>
   `
 })
 export class AppComponent  { 
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
  }
