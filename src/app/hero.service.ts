1d4d2d5aef0b187bfe4ad4efe944e2bc5a2dc3b4
Sherly Chan <sherlyc@gmail.com>
2017-06-25 17:41:34 +1200

    mock heroes service working

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,5 +5,7 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-    getHeroes(): void {} // stub
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
+ 
+export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -11,4 +11,4 @@ const HEROES: Hero[] = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
-]
+];
