0c8f5888dd95f0c4c8d58069b5cdec35573af7ff
Sherly Chan <sherlyc@gmail.com>
2017-06-25 23:40:40 +1200

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,31 @@ import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
-  providers:    [ HeroService],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+    { 
+      path: 'heroes',
+      component: HeroesComponent
+    }
+  ]) 
+],
+  declarations: [ 
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent 
+  ],
+  providers:  [ 
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
