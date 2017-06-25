49669cc4d27c7e141b2e99cb224d59bb7e2cea09
Sherly Chan <sherlyc@gmail.com>
2017-06-26 09:51:46 +1200

    added getHero service

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,6 +6,7 @@ import { HeroService } from './hero.service'
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
 })
+
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input} from '@angular/core';
-import { Hero } from './hero';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
 
 
 @Component({
@@ -18,6 +22,16 @@ import { Hero } from './hero';
   </div>`
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     @Input() hero: Hero;
+    constructor(
+      private heroService: HeroService,
+      private route: ActivatedRoute,
+      private location: Location
+    ) {}
+    ngOnInit(): void {
+      this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+    }
 }
